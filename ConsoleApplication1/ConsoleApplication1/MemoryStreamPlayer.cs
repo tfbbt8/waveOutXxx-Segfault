2a8f34cde81235a1838c4aa12c37314e9587e8db
tfbbt8 <tfbbt8@mail.missouri.edu>
2016-09-22 08:32:25 -0600

    Init Commit

---
+++ ConsoleApplication1/ConsoleApplication1/MemoryStreamPlayer.cs
@@ -0,0 +1,107 @@
+﻿using KnightRyderAudioPlayback;
+using System;
+using System.IO;
+using System.Timers;
+
+namespace WaveOutError
+{
+    class MemoryStreamPlayer
+    {
+        // File to play in memory
+        private MemoryStream MStream;
+        private BinaryWriter MStreamWriter;
+        private BinaryReader MStreamReader;
+
+        // Audio player
+        private WaveOutHandler wav = new WaveOutHandler();
+
+        // Wave Header offset in bytes
+        public int HeaderOffset { get; set; } = 44;
+
+        // Timer to buffer more samples for waveout
+        private Timer timer;
+
+        // Lock
+        private object lk = new object();
+
+        public MemoryStreamPlayer()
+        {
+            MStream = new MemoryStream();
+            MStreamWriter = new BinaryWriter(MStream);
+            MStreamReader = new BinaryReader(MStream);
+
+            timer = new Timer();
+            timer.Elapsed += TimerElapsed;
+            timer.AutoReset = true;
+            timer.Interval = 50;
+        }
+
+        // Queue any new samples
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            lock (lk)
+            {
+                // Error checking
+                if (wav == null || MStream == null) return;
+
+                // No new data in stream
+                if (MStream.Position >= MStream.Length) return;
+
+                // Byte buffer to read into from memory stream
+                byte[] bbuffer = new byte[MStream.Length - MStream.Position];
+
+                // Read in bytes from position to end
+                MStreamReader.Read(bbuffer, 0, (int)(MStream.Length - MStream.Position));
+
+                // Short buffer for waveout
+                short[] buffer = new short[bbuffer.Length / sizeof(short)];
+
+                // Copy bytes to short buffer
+                Buffer.BlockCopy(bbuffer, 0, buffer, 0, bbuffer.Length);
+
+                // Write samples to waveout
+                wav.Write(buffer, (uint)buffer.Length / 2);
+
+                // Advance position to end
+                MStream.Position = MStream.Length;
+            }
+        }
+
+        // MemoryStream operations
+        public void Write(short[] buffer)
+        {
+            lock (lk)
+            {
+                long save = MStream.Position;
+
+                MStream.Position = MStream.Length;
+                byte[] bbuffer = new byte[buffer.Length * sizeof(short)];
+                Buffer.BlockCopy(buffer, 0, bbuffer, 0, bbuffer.Length);
+                MStreamWriter.Write(bbuffer, 0, bbuffer.Length);
+
+                MStream.Position = save;
+            }
+        }
+
+        // Playback operations
+        public void Stop()
+        {
+            lock (lk)
+            {
+                wav.Close();
+                wav = null;
+            }
+        }
+
+        public void Reset()
+        {
+            wav.Reset();
+        }
+
+        public void Stream()
+        {
+            if (wav == null) wav = new WaveOutHandler();
+            timer.Start();
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Runtime;
+using System.Windows.Threading;
+
+namespace WaveOutError
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                MemoryStreamPlayer msp = new MemoryStreamPlayer();
+
+                Action<string> burst = new Action<string>((string file) =>
+                {
+                    // Open file and reader (16 bit 48000 hz PCM wav)
+                    FileStream fs = new FileStream(file, FileMode.Open);
+                    BinaryReader br = new BinaryReader(fs);
+
+                    // Buffers for whole file and PCM only
+                    byte[] bbuffer = new byte[fs.Length];
+                    short[] buffer = new short[(bbuffer.Length - 44) / 2];
+
+                    // Read in whole file
+                    br.Read(bbuffer, 0, bbuffer.Length);
+
+                    // Copy PCM into buffer without header
+                    Buffer.BlockCopy(bbuffer, 44, buffer, 0, bbuffer.Length - 44);
+
+                    Console.WriteLine("Loop gen starting");
+                    // i is bytes
+                    for (int i = 0; i < buffer.Length * sizeof(short); i += (2432 * 4))
+                    {
+                        short[] buf = new short[2432 * 2];
+                        Buffer.BlockCopy(buffer, i, buf, 0, buf.Length * sizeof(short));
+                        msp.Write(buf);
+
+                        // Simulate interval on radio
+                        Thread.Sleep(50);
+                    }
+                    Console.WriteLine("Loop gen done");
+                });
+
+                // Generator
+                Thread generator_loop = new Thread(() =>
+                {
+                    burst(@"93300_2016-09-19_085439.wav");
+                });
+
+                Thread generator_once = new Thread(() =>
+                {
+                    // Open file and reader (16 bit 48000 hz PCM wav)
+                    FileStream fs = new FileStream(@"93300_2016-09-19_112552.wav", FileMode.Open);
+                    byte[] bbuffer = new byte[fs.Length];
+                    fs.Read(bbuffer, 0, bbuffer.Length);
+                    short[] sbuffer = new short[(bbuffer.Length - 44) / sizeof(short)];
+                    Buffer.BlockCopy(bbuffer, 44, sbuffer, 0, (bbuffer.Length - 44));
+
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Starting gen once");
+                    msp.Write(sbuffer);
+                    Console.WriteLine("Gen Once done");
+                });
+
+                // Start checking for new audio samples and play as they come
+                msp.Stream();
+
+                generator_once.Start();
+                generator_loop.Start();
+                
+                // Allow audio to play and problems to occur
+                Thread.Sleep(20000);
+
+                generator_loop.Join();
+                generator_once.Join();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
